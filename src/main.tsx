d6cc6b712b28accf6d9870a46d0ef546811d4415
Douglas Toledo <dwtoledo@outlook.com>
2024-08-10 10:05:56 -0400

    Create and Implement Global CSS
    Add PT Sans and PT Sans Caption Font from Google

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 
-import "./reset.css"
+import './reset.css'
+import './global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
