676d8282a1a7806a52b1d928ec68c36eb5e70084
Douglas Toledo <dwtoledo@outlook.com>
2024-08-12 19:14:59 -0400

    Implement Event Covers on Character Details Component
    Implement Series Covers on Character Details Component

--- src/components/CharacterDetailsCard/index.tsx
+++ src/components/CharacterDetailsCard/index.tsx
@@ -1,16 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @stylistic/max-len */
-import { v4 as uuidv4 } from 'uuid'
 import styles from './style.module.css'
+import { useEffect, useState, useCallback } from 'react'
+import md5 from 'crypto-js/md5'
 
 interface CharacterDetailsCardProps {
-  avatarUrl: string
-  name: string
-  description: string
-  events: { items: { name: string }[] }
-  series: { items: { name: string }[] }
+  avatarUrl: string;
+  name: string;
+  description: string;
+  events: { items: { name: string; resourceURI: string; thumbnail: string }[] };
+  series: { items: { name: string; resourceURI: string; thumbnail: string }[] };
 }
 
-export function CharacterDetailsCard({ avatarUrl, name, description, events, series }: CharacterDetailsCardProps) {
+export function CharacterDetailsCard({
+  avatarUrl,
+  name,
+  description,
+  events,
+  series,
+}: CharacterDetailsCardProps) {
+  const [eventData, setEventData] = useState(events.items)
+  const [serieData, setSerieData] = useState(series.items)
+
+  const fetchThumbnails = useCallback(async (items: any[]) => {
+    const updatedItems = await Promise.all(
+      items.map(async (item) => {
+        const thumbnail = await getThumbnailByUrl(item.resourceURI)
+        return { ...item, thumbnail }
+      }),
+    )
+    return updatedItems
+  }, [])
+
+  useEffect(() => {
+    const fetchEventThumbnails = async () => {
+      if (events.items?.length) {
+        const updatedEvents = await fetchThumbnails(events.items)
+        setEventData(updatedEvents)
+      }
+    }
+    fetchEventThumbnails()
+  }, [events, fetchThumbnails])
+
+  useEffect(() => {
+    const fetchSerieThumbnails = async () => {
+      if (series.items?.length) {
+        const updatedSeries = await fetchThumbnails(series.items)
+        setSerieData(updatedSeries)
+      }
+    }
+    fetchSerieThumbnails()
+  }, [series, fetchThumbnails])
+
+  async function getThumbnailByUrl(resourceURI: string) {
+    const publicKey = import.meta.env.VITE_API_PUBLIC_KEY
+    const privateKey = import.meta.env.VITE_API_PRIVATE_KEY
+    const timestamp = new Date().getTime()
+    const hash = md5(timestamp + privateKey + publicKey).toString()
+    const url = `${resourceURI}?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`
+
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error('Não foi possível obter a imagem do evento!')
+      }
+      const data = await response.json()
+      const { path, extension } = data.data.results[0].thumbnail
+      return `${path}.${extension}`
+    } catch (error: any) {
+      console.error('Fetch error:', error)
+      return ''
+    }
+  }
+
   return (
     <div className={styles.card}>
       <figure className={styles.card_picture}>
@@ -20,19 +82,41 @@ export function CharacterDetailsCard({ avatarUrl, name, description, events, ser
         </figcaption>
       </figure>
 
-      <div className={styles.card_events}>
+      <section className={styles.card_events}>
         <h2 className={styles.card_subtitle}>Eventos:</h2>
-        {events.items.length
-          ? events.items.map(event => <p key={uuidv4()}>{event.name}</p>)
-          : <span>{name} não aparece em nenhum evento.</span>}
-      </div>
+        {eventData.length
+          ? (
+              eventData.map((event) => (
+                <article key={event.resourceURI} className={styles.coverContainer}>
+                  {event.thumbnail
+                    ? <img src={event.thumbnail} alt={`Capa do evento ${event.name}`} />
+                    : <span className={styles.loadingCoverMessage}>Carregando capa...</span>}
+                  <p>{event.name}</p>
+                </article>
+              ))
+            )
+          : (
+            <span>{name} não aparece em nenhum evento.</span>
+            )}
+      </section>
 
-      <div className={styles.card_series}>
+      <section className={styles.card_series}>
         <h2 className={styles.card_subtitle}>Séries:</h2>
-        {series.items.length
-          ? series.items.map(serie => <p key={uuidv4()}>{serie.name}</p>)
-          : <span>{name} não aparece em nenhuma série.</span>}
-      </div>
+        {serieData.length
+          ? (
+              serieData.map((serie) => (
+                <article key={serie.resourceURI} className={styles.coverContainer}>
+                  {serie.thumbnail
+                    ? <img src={serie.thumbnail} alt={`Capa da série ${serie.name}`} />
+                    : <span className={styles.loadingCoverMessage}>Carregando capa...</span>}
+                  <p>{serie.name}</p>
+                </article>
+              ))
+            )
+          : (
+            <span>{name} não aparece em nenhuma série.</span>
+            )}
+      </section>
     </div>
   )
 }
